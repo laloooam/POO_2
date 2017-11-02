8f812772f25160b873b10f1fb1676557ff677639
laloooam <eduard00adasme@gmail.com>
2017-11-02 13:55:10 -0400

    Se creo la clase hace frio y otro sout en la main

---
+++ Eduardo_adasme/src/eduardo_adasme/Hacefrio.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eduardo_adasme;
+
+public class Hacefrio {
+    private int caca;
+
+    public Hacefrio(int caca) {
+        this.caca = caca;
+    }
+
+    public int getCaca() {
+        return caca;
+    }
+
+    public void setCaca(int caca) {
+        this.caca = caca;
+    }
+    
+}
